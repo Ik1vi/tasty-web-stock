9c86177e69b58e15bc6b3e0fa3844199c2202681
Irene Veselova <ik1vikuz@gmail.com>
2021-02-17 19:42:50 +0300

    adaptive scss

--- src/index.js
+++ src/index.js
@@ -11,26 +11,3 @@ ReactDOM.render(
 if (module.hot) {
     module.hot.accept();
 }
-
-
-// // containers
-
-// btnLikesEl.addEventListener('click', function (ev) {
-//     ev.preventDefault();
-//     bodyEl.classList.add('liked-container-open', 'js-fixed');
-//     // bodyEl.style.height=window.height() + window.scrollTop();
-//     // bodyEl.style.marginTop = window.scrollTop()*-1;
-// });
-
-// picItemEl.addEventListener('click', function (ev) {
-//     ev.preventDefault();
-//     bodyEl.classList.add('picture-container-open', 'js-fixed');
-// });
-
-// document.addEventListener('mousedown', function(e) {
-// 	if(!e.target.closest('.js-picture-container-wrapper') && !e.target.closest('.js-liked-wrapper') ) {
-//         bodyEl.classList.remove('liked-container-open', 'picture-container-open', 'js-fixed');
-// 	}
-// });
-
-
